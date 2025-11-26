2ae9f3c24d8bda97de2b09bae6374a6ebbc6a7d1
Jonathan <jonathanhillman285@gmail.com>
2025-11-26 10:39:40 +0200

    work on motor

--- Projects/run_motor.py
+++ Projects/run_motor.py
@@ -1,12 +1,24 @@
 # pyright: reportMissingImports=false
 
 from machine import Pin
-
+import time
 IN1 = Pin(18, Pin.OUT)
 IN2 = Pin(19, Pin.OUT)
+BOTTOn_pin = Pin(5, Pin.IN)
+
+while True:
+    if BOTTOn_pin.value() == 1:
+        IN1.toggle()
+        time.sleep(0.3)
+
+
+
+'''
+
 
 # Forward full power (assuming ENA is hard-wired to +5V)
 IN1.value(1)
 IN2.value(0)
 
 print("Motor running forward at full power")
+'''
