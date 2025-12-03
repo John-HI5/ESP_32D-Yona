71b248382a5d35b603eed7a3b253af7d00c80d28
Jonathan <jonathanhillman285@gmail.com>
2025-12-03 10:56:55 +0200

    work on pwm triple motor code

--- Projects/tripledrivetrain.py
+++ Projects/tripledrivetrain.py
@@ -1,39 +1,37 @@
 # pyright: reportMissingImports=false
 from machine import Pin, PWM
 import time
+from machine import Pin, PWM
+import time
 
+# One motor example
+IN1 = PWM(Pin(18))
+IN2 = Pin(19, Pin.OUT)
 
-motors = {
-    1: (Pin(18, Pin.OUT), Pin(19, Pin.OUT)),
-    2: (Pin(21, Pin.OUT), Pin(22, Pin.OUT)),
-    3: (Pin(23, Pin.OUT), Pin(5, Pin.OUT))
-}
+IN1.freq(2000)  # PWM frequency
 
-FREQ = 2000
-
-def change_motor(power, direction, motor):
+def change_motor(power, direction):
     """
-    power: 0–1 (duty)
+    power: 0.0–1.0
     direction: 0=left, 1=right, 2=stop
-    motor: motor number (1,2,3)
     """
 
-    IN1, IN2 = motors[motor]
+    duty = int(power * 1023)
 
-    duty = int(power * 1023)  # 10bit PWM
-
-    if direction == 0:       # LEFT
-        IN1.value(1)
+    if direction == 0:      # left
         IN2.value(0)
+        IN1.duty(duty)
 
-    elif direction == 1:     # RIGHT
-        IN1.value(0)
+    elif direction == 1:    # right
+        
         IN2.value(1)
+        IN1.duty(duty)
 
-    else:                    # STOP
-        IN1.value(0)
+    else:                  # stop
+        IN1.duty(0)
         IN2.value(0)
-        return
 
-
-    pwm = PWM(IN1, freq=FREQ, duty=duty)
+for p in [0.01, 0.1, 0.2, 0.3, 0.5, 1]:
+    change_motor(p, 1, 1)
+    print("Power:", p)
+    time.sleep(1)
