286f9e4d8f00b8153ef02ede449a058fe94cae40
Jonathan <jonathanhillman285@gmail.com>
2025-12-09 12:05:47 +0200

    work on PWM

--- Projects/run_motor.py
+++ Projects/run_motor.py
@@ -6,6 +6,29 @@ IN1 = Pin(18, Pin.OUT)
 IN2 = Pin(19, Pin.OUT)
 BOTTOn_pin = Pin(5, Pin.IN)
 
+
+def Tpwm(duration, Hz, percent):
+    end_time = time.time() + duration   
+    duration2 = 1/Hz
+
+    while time.time() < end_time: #big timer - duration
+        end_time2 = time.time() + duration2
+
+        while time.time() < end_time2:
+            IN2.value(1)
+            time.sleep(Hz * percent)
+            IN2.value(0)
+
+
+
+
+
+from machine import Pin
+import time
+IN1 = Pin(18, Pin.OUT)
+IN2 = Pin(19, Pin.OUT)
+BOTTOn_pin = Pin(5, Pin.IN)
+
 while True:
     if BOTTOn_pin.value() == 1:
         IN1.toggle()
