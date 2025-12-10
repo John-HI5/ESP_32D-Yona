a7b68a9bb2af1221e1c34f4d2405229bb63bb93b
Jonathan <jonathanhillman285@gmail.com>
2025-12-10 09:20:29 +0200

    finished PWN func!

--- Projects/run_motor.py
+++ Projects/run_motor.py
@@ -2,27 +2,35 @@
 
 from machine import Pin
 import time
+
 IN1 = Pin(18, Pin.OUT)
 IN2 = Pin(19, Pin.OUT)
-BOTTOn_pin = Pin(5, Pin.IN)
-
+BUTTON_pin = Pin(5, Pin.IN)
 
 def Tpwm(duration, Hz, percent):
-    end_time = time.time() + duration   
-    duration2 = 1/Hz
+    period = 1_000_000 // Hz                # period in microseconds
+    on_time = int(period * percent)         # % ON
+    off_time = period - on_time
 
-    while time.time() < end_time: #big timer - duration
-        end_time2 = time.time() + duration2
+    end_time = time.ticks_us() + duration * 1_000_000
 
-        while time.time() < end_time2:
-            IN2.value(1)
-            time.sleep(Hz * percent)
-            IN2.value(0)
+    while time.ticks_diff(end_time, time.ticks_us()) > 0:
+        # ON time
+        IN2.value(1)
+        time.sleep_us(on_time)
+
+        # OFF time
+        IN2.value(0)
+        time.sleep_us(off_time)
+x=1.0
+for i in range(8):
+    x=x-0.1
+    Tpwm(2, 20_000, x)
+    time.sleep(0.4)
 
 
 
-
-
+'''
 from machine import Pin
 import time
 IN1 = Pin(18, Pin.OUT)
@@ -35,3 +43,4 @@ while True:
         time.sleep(0.3)
         print("moving")
     print(BOTTOn_pin.value())
+'''
