1650a9485aad8a8b3ce19c70452dab23e3954cd8
Jonathan <jonathanhillman285@gmail.com>
2025-12-10 18:28:59 +0200

    work on tripleDT

--- Projects/tripledrivetrain.py
+++ Projects/tripledrivetrain.py
@@ -1,20 +1,38 @@
 # pyright: reportMissingImports=false
-from machine import Pin, PWM
+#from machine import Pin, PWM
 import time
 import math
-from PWM_func import Tpwm
+#from PWM_func.py import Tpwm
 
 def input_stick (pin1, pin2): #needs to normalize the stick values and return x,y,and strength(lenth is Pythagoras)
     print("make me")
     x = 300
     y = 400
     v = (y**2 + x**2)** 0.5
-    return x,y,v
+    return (x,y,v)
 
-def calc_alpha (X, Y, L): #calc M with x and y and then the alpha 
+
+def calc_alpha (X, Y, V): #calc M with x and y and then the alpha 
     m = Y / X
-    angle_degrees = math.degrees(math.atan(m)) #tan in D for m to find alpha
+    alpha = math.degrees(math.atan(m)) #tan in D for m to find alpha
+    return alpha , V
 
-def q (V, alpha, length):
-    print("make me")
-    
+def q (alpha, V): #returns q
+
+     # calc 
+    q = V * math.sin(math.radians(90 - alpha))
+    print(q)
+    return q
+
+def playall ():
+    aa = input_stick (1, 2)
+    print("finished aa")
+    bb = calc_alpha(aa)
+    print("finished bb")
+    cc = q(bb)
+    print("finished cc")
+    print (cc)
+    return cc
+
+
+playall()
